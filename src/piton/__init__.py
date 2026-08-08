b9e4eceb3136633b127c8fa256a6d2ae61b0981a
Nick Mercer <nick-mercer@agents.tarxan.io>
2026-08-08 10:43:23 -0500

    feat: admit bounded autonomous engineering requests

--- src/piton/__init__.py
+++ src/piton/__init__.py
@@ -1,5 +1,14 @@
 """Piton Mechanical CAD MVI foundation."""
 
+from .admission import (
+    AdmissionDecision,
+    AdmissionPolicy,
+    AutonomyGrant,
+    Effect,
+    EngineeringRequest,
+    PrincipalContext,
+    admit_engineering_request,
+)
 from .model import (
     BuildAttempt,
     BuildStatus,
@@ -13,15 +22,22 @@ from .model import (
 from .revision import DesignRevision, canonical_json_bytes, compute_revision_id
 
 __all__ = [
+    "AdmissionDecision",
+    "AdmissionPolicy",
+    "AutonomyGrant",
     "BuildAttempt",
     "BuildStatus",
     "ChangeProposal",
     "DesignRevision",
     "DraftExport",
+    "Effect",
+    "EngineeringRequest",
     "EvidenceClosure",
     "ReviewDisposition",
+    "PrincipalContext",
     "TruthBoundary",
     "validate_lifecycle",
     "canonical_json_bytes",
     "compute_revision_id",
+    "admit_engineering_request",
 ]
